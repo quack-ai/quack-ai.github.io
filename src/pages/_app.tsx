59246f9125ca147631fc2601fdd8411019d54cfa
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-08-17 00:25:54 -0700

    feat: Adds Vercel analytics (#4)
    * feat: Adds Vercel analytics
    
    * style: Fixes lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
