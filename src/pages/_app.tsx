a708815eedab449ce1b53b1d63a99c292bb44cc6
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-09-29 10:14:18 +0200

    refactor: Reordered project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
